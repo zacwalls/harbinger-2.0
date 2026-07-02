f8a56532514ef25624b785ff6c017eb09435d1df
Zack <zwallace0790@gmail.com>
2026-07-01 21:36:04 -0700

    I vibe coded the figma design for the main page to get the ball rolling a bit.

---
+++ src/tailwind.config.js
@@ -0,0 +1,22 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      spacing: {
+        '1': '1rem',
+        '2': '1.5rem',
+        '3': '2rem',
+        '4': '2.5rem',
+        '5': '3rem',
+        '6': '3.5rem',
+        '7': '4rem',
+        '8': '4.5rem',
+        '9': '5rem',
+        '10': '5.5rem',
+        '11': '6rem',
+        '12': '6.5rem',
+        '13': '7rem',
+      }
+    }
+  }
+}
