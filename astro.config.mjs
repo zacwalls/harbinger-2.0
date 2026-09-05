75615767f333144ec811970af7d0692cf4216067
Zach Wallace <zach@Zachs-MacBook-Air.local>
2026-09-04 23:09:17 -0700

    Trying to solve github pages assets not loading x2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+const isProd = process.env.NODE_ENV === 'production';
 
 // https://astro.build/config
 export default defineConfig({
     output: "static",
+    base: isProd ? "/harbinger-2.0/" : "/",
     image: {
       domains: ['localhost', '://onrender.com', 'zacwalls.github.io/harbinger-2.0/'],
     },
