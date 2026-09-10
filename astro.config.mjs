b4ff8b86bb26205d94ec15975514532b6eb1aec9
Zach Wallace <zach@Zachs-MacBook-Air.local>
2026-09-10 00:04:01 -0700

    Fixing staging build

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ const isProd = process.env.NODE_ENV === 'production';
 // https://astro.build/config
 export default defineConfig({
     output: "static",
-    base: isProd ? "/harbinger-2.0/" : "/",
+    site: isProd ? "https://harbingermma.com" : "https://zacwalls.github.io/",
+    base: isProd ? "/" : "/harbinger-2.0",
     image: {
       domains: ['localhost', '://onrender.com'],
     },
