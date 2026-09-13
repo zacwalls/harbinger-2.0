5c8374a43290dfc0bd72873a1a45c3511bca6850
Zach Wallace <zach@Zachs-MacBook-Air.local>
2026-09-12 21:35:06 -0700

    Getting ready to start on schedule page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import preact from "@astrojs/preact";
 
 const isStaging = process.env.NODE_ENV === 'staging';
 
 // https://astro.build/config
 export default defineConfig({
-    output: "static",
-    site: isStaging ? "https://zacwalls.github.io/" : "https://harbingermma.com",
-    base: isStaging ? "/harbinger-2.0" : "/",
-    image: {
-      domains: ['localhost', '://onrender.com'],
-    },
-    vite: {
-      plugins: [tailwindcss()],
-    },
+  output: "static",
+  site: isStaging ? "https://zacwalls.github.io/" : "https://harbingermma.com",
+  base: isStaging ? "/harbinger-2.0/" : "/",
+
+  image: {
+    domains: ['localhost', '://onrender.com'],
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [preact()]
 });
